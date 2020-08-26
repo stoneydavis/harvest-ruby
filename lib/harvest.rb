7d0e6768bbb0b8fbc7e6136cf43e13f00080ceea
Craig Davis <craig.davis@rackspace.com>
2020-08-26 14:07:55 -0500

    Refactor to abstract out the http client

--- lib/harvest.rb
+++ lib/harvest.rb
@@ -6,6 +6,7 @@ require 'rest-client'
 
 require 'harvest/version'
 require 'harvest/resources'
+require 'harvest/httpclient'
 
 def convert_to_sym(data)
   return data.map { |k, v| [k.to_sym, convert_to_sym(v)] }.to_h if data.respond_to?('keys')
@@ -32,14 +33,15 @@ module Harvest
     # @param personal_token [String] Harvest Personal token
     # @param admin_api [Boolean] Changes functionality of how the interface works
     def initialize(domain:, account_id:, personal_token:, admin_api: false)
-      @client = RestClient::Resource.new(
-        domain.chomp('/') + '/api/v2',
-        { headers: headers(personal_token, account_id) }
+      @client = Harvest::HTTPClient.new(
+        domain: domain,
+        account_id: account_id,
+        personal_token: personal_token
       )
       @admin_api = admin_api
       @factory = Harvest::ResourceFactory.new
       @state = ''
-      @active_user = @factory.user(JSON.parse(@client['/users/me'].get))
+      @active_user = @factory.user(@client.api_call('/users/me'))
     end
 
     # Change context to projects
@@ -82,7 +84,7 @@ module Harvest
       # binding.pry
       case @state
       when 'projects'
-        @projects = [@factory.project(api_call("projects/#{id}"))]
+        @projects = [@factory.project(@client.api_call("projects/#{id}"))]
         self
       end
     end
@@ -130,7 +132,7 @@ module Harvest
         # TODO: check if keys required are present and raise if not
         payload = time_entry_payload(kwargs)
         begin
-          api_call(
+          @client.api_call(
             'time_entries',
             http_method: 'post',
             payload: payload.to_json,
@@ -144,55 +146,6 @@ module Harvest
     end
 
     # private
-
-    # Make a api call to an endpoint.
-    # @api private
-    # @param path [String] Url path part omitting preceeding slash
-    # @param method [String] HTTP Method to call
-    # @param param [Hash] Query Params to pass
-    # @param data [Hash] Body of HTTP request
-    def api_call(path, http_method: 'get', param: nil, payload: nil, headers: nil)
-      # RestClient::Request.process_url_params is looking for query params in the headers obj,
-      headers ||= {}
-      headers['params'] = param
-      case http_method
-      when 'get'
-        http_resp = @client[path].get(headers)
-        JSON.parse(http_resp)
-      when 'post'
-        @client[path].post(payload, headers)
-      end
-    end
-
-    # Pagination through request
-    # @api private
-    # @param path [String] Url path part omitting preceeding slash
-    # @param method [String] HTTP Method to call
-    # @param param [Hash] Query Params to pass
-    # @param data [Hash] Body of HTTP request
-    def pagination(path, data_key, http_method: 'get', page_count: 1, param: nil, payload: nil, entries: nil)
-      # TODO: tail call optimization
-      param ||= {}
-      entries ||= []
-
-      param[:page] = page_count
-
-      page = api_call(path, http_method: http_method, param: param, payload: payload)
-      entries.concat(page[data_key])
-
-      return entries if page_count >= page['total_pages']
-
-      page_count += 1
-      pagination(
-        path,
-        data_key,
-        http_method: http_method,
-        page_count: page_count,
-        param: param,
-        payload: payload,
-        entries: entries
-      )
-    end
 
     # @api private
     def true_project(project)
@@ -222,7 +175,7 @@ module Harvest
     # @api private
     # All Projects
     def admin_projects
-      api_call('projects')['projects']
+      @client.api_call('projects')['projects']
         .map { |project| @factor.project(project) }
     end
 
@@ -237,7 +190,7 @@ module Harvest
     # @api private
     # Projects assigned to the specified user_id
     def project_assignments(user_id: active_user.id)
-      convert_to_sym(api_call("users/#{user_id}/project_assignments")['project_assignments'])
+      convert_to_sym(@client.api_call("users/#{user_id}/project_assignments")['project_assignments'])
         .map do |project|
           @factory.project_assignment(project)
         end
---
+++ lib/harvest/httpclient.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module Harvest
+  class HTTPClient
+    def initialize(domain:, account_id:, personal_token:, admin_api: false)
+      @client = RestClient::Resource.new(
+        domain.chomp('/') + '/api/v2',
+        { headers: headers(personal_token, account_id) }
+      )
+    end
+
+    # Make a api call to an endpoint.
+    # @api public
+    # @param path [String] Url path part omitting preceeding slash
+    # @param method [String] HTTP Method to call
+    # @param param [Hash] Query Params to pass
+    # @param data [Hash] Body of HTTP request
+    def api_call(path, http_method: 'get', param: nil, payload: nil, headers: nil)
+      # RestClient::Request.process_url_params is looking for query params in the headers obj,
+      headers ||= {}
+      headers['params'] = param
+      case http_method
+      when 'get'
+        http_resp = @client[path].get(headers)
+        JSON.parse(http_resp)
+      when 'post'
+        @client[path].post(payload, headers)
+      end
+    end
+
+    # Pagination through request
+    # @api public
+    # @param path [String] Url path part omitting preceeding slash
+    # @param method [String] HTTP Method to call
+    # @param param [Hash] Query Params to pass
+    # @param data [Hash] Body of HTTP request
+    def pagination(path, data_key, http_method: 'get', page_count: 1, param: nil, payload: nil, entries: nil)
+      # TODO: tail call optimization
+      param ||= {}
+      entries ||= []
+
+      param[:page] = page_count
+
+      page = api_call(path, http_method: http_method, param: param, payload: payload)
+      entries.concat(page[data_key])
+
+      return entries if page_count >= page['total_pages']
+
+      page_count += 1
+      pagination(
+        path,
+        data_key,
+        http_method: http_method,
+        page_count: page_count,
+        param: param,
+        payload: payload,
+        entries: entries
+      )
+    end
+
+  end
+end
