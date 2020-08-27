2ff830437818ee3ee3fb72556147292974f3eafd
Craig Davis <craig.davis@rackspace.com>
2020-08-27 11:42:03 -0500

    Added disabled tests for harvest client until i can mock the Rest::Client

--- spec/harvest_spec.rb
+++ spec/harvest_spec.rb
@@ -2,8 +2,29 @@ RSpec.describe Harvest do
   it "has a version number" do
     expect(Harvest::VERSION).not_to be nil
   end
+  config = {
+    domain: 'https://exampledomain.harvestapp.com',
+    account_id: 'example_account_id',
+    personal_token: 'example_personal_token'
+  }
 
-  # it "does something useful" do
-  #   expect(false).to eq(true)
+  # it 'create harvest client' do
+  #   expect(Harvest::Client.new(config))
   # end
+
+  # let(:harvest) { Harvest::Client.new(config) }
+  context 'harvest client' do
+    xit 'sets active_user' do
+      expect(harvest.active_user.id).to be(123_456_789_012)
+    end
+    xit 'sets client' do
+      expect(harvest.client).to be_instance_of(Harvest::HTTP::Client)
+    end
+    xit 'sets state' do
+      expect(harvest.state).to eq('')
+    end
+    xit 'sets project state' do
+      expect(harvest.projects.state).to eq('projects')
+    end
+  end
 end
