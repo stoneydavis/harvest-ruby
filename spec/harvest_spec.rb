ed35cf8d9f65925e4e20cb7bbb774f560f0e32d9
Craig Davis <craig.davis@rackspace.com>
2020-08-28 16:40:30 -0500

    Fixed tests for client

--- spec/harvest_spec.rb
+++ spec/harvest_spec.rb
@@ -1,37 +1,36 @@
+# frozen_string_literal: true
+
+require 'pry'
+
 RSpec.describe Harvest do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Harvest::VERSION).not_to be nil
   end
-  config = {
-    domain: 'https://exampledomain.harvestapp.com',
-    account_id: 'example_account_id',
-    personal_token: 'example_personal_token'
-  }
+  let(:config) do
+    {
+      domain: 'https://exampledomain.harvestapp.com',
+      account_id: 'example_account_id',
+      personal_token: 'example_personal_token'
+    }
+  end
 
   xit 'create harvest http client' do
     expect(Harvest::HTTP::Client.new(config))
   end
 
-  # TODO: create double of Harvest::HTTP::Client
-  let(:client) { Harvest::HTTP::Client.new(config) }
-
-  xit 'create harvest client' do
-    expect(Harvest::Client.new(client))
-  end
+  let(:client) { instance_double(Harvest::HTTP::Client, api_caller: Harvest::HTTP::ApiCall.new, paginator: Harvest::HTTP::Pagination.new, api_call: {id: 123_456_789_012}, pagination: {}) }
 
   let(:harvest) { Harvest::Client.new(client) }
   context 'harvest client' do
-    xit 'sets active_user' do
+    it 'create harvest client' do
+      expect(Harvest::Client.new(client))
+    end
+
+    it 'sets active_user' do
       expect(harvest.active_user.id).to be(123_456_789_012)
     end
-    xit 'sets client' do
-      expect(harvest.client).to be_instance_of(Harvest::HTTP::Client)
-    end
-    xit 'sets state' do
-      expect(harvest.state).to eq('')
-    end
-    xit 'sets project state' do
-      expect(harvest.projects.state).to eq('projects')
+    it 'sets project state' do
+      expect(harvest.projects.state[:default]).to eq(:projects)
     end
   end
 end
