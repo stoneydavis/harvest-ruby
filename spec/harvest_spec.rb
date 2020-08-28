5bf8d663727e63fde0dc11e0cb6df35967e67cf5
Craig Davis <craig.davis@rackspace.com>
2020-08-28 13:56:00 -0500

    Uncommented the test but disabled them

--- spec/harvest_spec.rb
+++ spec/harvest_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Harvest do
     personal_token: 'example_personal_token'
   }
 
-  # it 'create harvest client' do
-  #   expect(Harvest::Client.new(config))
-  # end
+  xit 'create harvest client' do
+    expect(Harvest::Client.new(config))
+  end
 
-  # let(:harvest) { Harvest::Client.new(config) }
+  let(:harvest) { Harvest::Client.new(config) }
   context 'harvest client' do
     xit 'sets active_user' do
       expect(harvest.active_user.id).to be(123_456_789_012)
