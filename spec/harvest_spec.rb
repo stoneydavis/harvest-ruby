7a15c9d13049e97c6a6bec69893d4dc49bf5772b
Craig Davis <craig.davis@rackspace.com>
2020-09-01 15:12:57 -0500

    Reduce testing overlay

--- spec/harvest_spec.rb
+++ spec/harvest_spec.rb
@@ -10,52 +10,49 @@ RSpec.describe Harvest do
       personal_token: 'example_personal_token'
     }
   end
-  let(:harvest) { Harvest::Client.new(config) }
+  let(:harvest) do
+    body = { 'id' => 1_234_567 }
+    stub_request(:get, "#{config[:domain]}/api/v2/users/me")
+      .to_return(body: body.to_json, status: 200)
+    Harvest::Client.new(config)
+  end
 
   it 'has a version number' do
     expect(Harvest::VERSION).not_to be nil
   end
 
-  it 'creates harvest client' do
-    body = { 'id' => 1_234_567 }
-    stub_request(:get, "#{config[:domain]}/api/v2/users/me")
-      .to_return(body: body.to_json, status: 200)
-    expect(Harvest::Client.new(config).active_user.id).to eq(body['id'])
-  end
-
-  context 'harvest' do
-    before do
-      body = { 'id' => 1_234_567 }
-      stub_request(:get, "#{config[:domain]}/api/v2/users/me")
-        .to_return(body: body.to_json, status: 200)
-      @harvest = Harvest::Client.new(config)
+  context 'with harvest' do
+    it 'has active user id' do
+      expect(harvest.active_user.id).to eq(1_234_567)
     end
 
     it 'change active state to projects' do
-      expect(@harvest.projects.state[:active]).to eq(:projects)
+      expect(harvest.projects.state[:active]).to eq(:projects)
     end
 
     it 'change active state to time_entry' do
-      expect(@harvest.time_entry.state[:active]).to eq(:time_entry)
+      expect(harvest.time_entry.state[:active]).to eq(:time_entry)
     end
 
     it 'find project' do
       body = { 'id' => 983_754 }
       stub_request(:get, "#{config[:domain]}/api/v2/projects/983754")
         .to_return(body: body.to_json, status: 200)
-      expect(@harvest.projects.find(body['id']).state[:projects][0].id).to eq(body['id'])
+      expect(harvest.projects.find(body['id']).state[:projects][0].id).to eq(body['id'])
     end
 
-    it 'select a projects' do
+    it 'select a project' do
       body = {
         project_assignments: [
           { 'id' => 349_832, project: { 'name' => 'Bob Co' } },
           { 'id' => 97_836_415, project: { name: 'George Co' } }
         ]
       }
-      stub_request(:get, "#{config[:domain]}/api/v2/users/#{@harvest.active_user.id}/project_assignments")
-        .to_return(body: body.to_json, status: 200)
-      projects = @harvest.projects.discover.select { |pa| pa.project.name == 'Bob Co' }
+      stub_request(
+        :get,
+        "#{config[:domain]}/api/v2/users/#{harvest.active_user.id}/project_assignments"
+      ).to_return(body: body.to_json, status: 200)
+      projects = harvest.projects.discover.select { |pa| pa.project.name == 'Bob Co' }
       expect(projects.state[:filtered][:projects][0].project.name).to eq('Bob Co')
     end
   end
