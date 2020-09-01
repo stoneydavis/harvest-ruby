e6c946bb208b33a06316bc4a31db80a984cea99c
Craig Davis <craig.davis@rackspace.com>
2020-09-01 14:45:05 -0500

    Added new tests

--- spec/harvest_spec.rb
+++ spec/harvest_spec.rb
@@ -17,35 +17,33 @@ RSpec.describe Harvest do
   end
 
   it 'creates harvest client' do
-    body = {
-      'id' => 1_234_567,
-      'first_name' => 'John',
-      'last_name' => 'Smith',
-      'email' => 'user@example.com',
-      'telephone' => '',
-      'timezone' => 'Central Time (US & Canada)',
-      'weekly_capacity' => 144_000,
-      'has_access_to_all_future_projects' => false,
-      'is_contractor' => false,
-      'is_admin' => false,
-      'is_project_manager' => false,
-      'can_see_rates' => false,
-      'can_create_projects' => false,
-      'can_create_invoices' => false,
-      'is_active' => true,
-      'calendar_integration_enabled' => true,
-      'calendar_integration_source' => 'outlook',
-      'created_at' => '2018-10-08T16:13:37Z',
-      'updated_at' => '2020-08-14T22:32:40Z',
-      'roles' => [],
-      'avatar_url' =>
-      'https://d3s3969qhosaug.cloudfront.net/default-avatars/1.png'
-    }
-
+    body = { 'id' => 1_234_567 }
     stub_request(:get, "#{config[:domain]}/api/v2/users/me")
       .to_return(body: body.to_json, status: 200)
     expect(Harvest::Client.new(config).active_user.id).to eq(body['id'])
   end
+
   context 'harvest' do
+    before do
+      body = { 'id' => 1_234_567 }
+      stub_request(:get, "#{config[:domain]}/api/v2/users/me")
+        .to_return(body: body.to_json, status: 200)
+      @harvest = Harvest::Client.new(config)
+    end
+
+    it 'change active state to projects' do
+      expect(@harvest.projects.state[:active]).to eq(:projects)
+    end
+
+    it 'change active state to time_entry' do
+      expect(@harvest.time_entry.state[:active]).to eq(:time_entry)
+    end
+
+    it 'find project' do
+      body = { 'id' => 983_754 }
+      stub_request(:get, "#{config[:domain]}/api/v2/projects/983754")
+        .to_return(body: body.to_json, status: 200)
+      expect(@harvest.projects.find(body['id']).state[:projects][0].id).to eq(body['id'])
+    end
   end
 end
